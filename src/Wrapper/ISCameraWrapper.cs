073e835fd2c9d2e33eaad40af3e39139e24a2dcc
Patrick Lang <lang.patrick92@gmail.com>
2023-01-02 14:50:12 +0100

    Implemented normalization of the Y rotation

--- src/Wrapper/ISCameraWrapper.cs
+++ src/Wrapper/ISCameraWrapper.cs
@@ -1,5 +1,6 @@
 ﻿namespace ISCameraMod.Wrapper
 {
+	using System;
 	using System.Linq;
 	using ISCameraMod.Model;
 	using UnityEngine;
@@ -15,7 +16,7 @@
 		private const float CameraMoveDuration = 0.5f;
 
 		private AnimatedCameraPosition _animatedCameraPosition;
-		
+
 		/// <summary>
 		/// Updates the movement of the camera.
 		/// </summary>
@@ -120,12 +121,14 @@
 
 			public AnimatedCameraPosition(CameraPosition from, CameraPosition to, float timeStart, float timeEnd)
 			{
+				var normalizedRotationY = NormalizeRotationY(from.RotationY, to.RotationY);
+
 				_timeEnd = timeEnd;
 				_animationPositionX = AnimationCurve.EaseInOut(timeStart, from.Position.x, timeEnd, to.Position.x);
 				_animationPositionY = AnimationCurve.EaseInOut(timeStart, from.Position.y, timeEnd, to.Position.y);
 				_animationPositionZ = AnimationCurve.EaseInOut(timeStart, from.Position.z, timeEnd, to.Position.z);
 				_animationRotationX = AnimationCurve.EaseInOut(timeStart, from.RotationX, timeEnd, to.RotationX);
-				_animationRotationY = AnimationCurve.EaseInOut(timeStart, from.RotationY, timeEnd, to.RotationY);
+				_animationRotationY = AnimationCurve.EaseInOut(timeStart, normalizedRotationY.From, timeEnd, normalizedRotationY.To);
 				_animationZoomLevel = AnimationCurve.EaseInOut(timeStart, from.ZoomLevel, timeEnd, to.ZoomLevel);
 
 				TargetPosition = to;
@@ -144,6 +147,55 @@
 					RotationY = _animationRotationY.Evaluate(time),
 					ZoomLevel = _animationZoomLevel.Evaluate(time)
 				};
+			}
+
+			/// <summary>
+			/// This methods normalizes the start and end position of the rotation around the Y axis. 
+			/// </summary>
+			/// <remarks>
+			/// The camera can be rotated between 0° and 360° (or 359°, since 0° and 360° are resulting in the same rotation).
+			/// If the player rotates the camera a few times, the rotation can be more than 360°, or less than 0°, depending on the rotation direction.
+			/// The animation may would rotate a few times before reaching the end position if this would not be taken into account.
+			/// The start and end position of the rotation are adapted to animate the shortest distance between the positions.
+			/// </remarks>
+			/// <param name="from">The start position.</param>
+			/// <param name="to">The end position.</param>
+			/// <returns>The adapted positions.</returns>
+			private (float From, float To) NormalizeRotationY(float from, float to)
+			{
+				// Step 1: Adapt the values to be between 0 and 360
+				while (from > 360)
+				{
+					from -= 360;
+				}
+
+				while (from < 0)
+				{
+					from += 360;
+				}
+
+				while (to > 360)
+				{
+					to -= 360;
+				}
+
+				while (to < 0)
+				{
+					to += 360;
+				}
+
+				// Step 2: Check if it is shorter to animate from the start position to the end position, or vice versa
+				var diff = Math.Abs(to - from);
+				if (diff > 180)
+				{
+					// It is shorter to animate "the other way round"
+					// Let's say, the From position is 10° and the To position is 350°, so it takes only 20° to go from 10 to 0, and then from 360 to 350
+					// An easy way to accomplish that is to simply adapt the To position by either adding or subtracting 360°, depending on the shorter direction
+					// In the mention example, we would then animate from 10° to -10°
+					to += (to > from ? -360 : 360);
+				}
+
+				return (from, to);
 			}
 		}
 	}
